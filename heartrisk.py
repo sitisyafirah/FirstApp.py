74c1aefd78335d189ffc2254129be35fdf9b7197
sitisyafirah <94594600+sitisyafirah@users.noreply.github.com>
2021-11-21 23:57:09 +0800

    Update heartrisk.py

--- heartrisk.py
+++ heartrisk.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 st.header("Heart Disease Prediction Analysis")
 st.write("In this dataset, there are many cases  with their ASCVDs."
          
