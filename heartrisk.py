e6c7627b68cbf413c6bc158da3ff9248a1a5a643
sitisyafirah <94594600+sitisyafirah@users.noreply.github.com>
2021-12-06 17:08:14 +0800

    Update heartrisk.py

--- heartrisk.py
+++ heartrisk.py
@@ -37,49 +37,49 @@ elif option=='BMI CALCULATOR':
       
 elif option=='CALORIE CALCULATOR':
       #def user_info():
-       age = st.number_input("What is your age: ")
-       gender = st.text_input("What is your gender (male/female): ")
-       weight = st.number_input("What is your weight: ")
-       height = st.number_input("What is your height in inches: ")
-       bmr_result = float(bmr_result)
-       if gender == 'male':
-          bmr_result = 66 + (6.2 * height) + (12.7 * weight) - (6.76 * age)
-       elif gender == 'female':
-	  bmr_result = 11
-		#655.1 + (4.35 * height) + (4.7 * weight) - (4.7 * age)
+      age = st.number_input("What is your age: ")
+      gender = st.text_input("What is your gender (male/female): ")
+      weight = st.number_input("What is your weight: ")
+      height = st.number_input("What is your height in inches: ")
+      bmr_result = float(bmr_result)
+      if gender == 'male':
+        bmr_result = 66 + (6.2 * height) + (12.7 * weight) - (6.76 * age)
+      elif gender == 'female':
+	bmr_result = 655.1 + (4.35 * height) + (4.7 * weight) - (4.7 * age)
+		
         #BMR = 665 + (9.6 X 69) + (1.8 x 178) – (4.7 x 27)
     #def calculate_activity(bmr_result): 
-       activity_level = st.text_input("What is your activity level (none, light, moderate, heavy, or extreme): ")
+      activity_level = st.text_input("What is your activity level (none, light, moderate, heavy, or extreme): ")
        
-       if activity_level == 'none':
+      if activity_level == 'none':
             activity_level = 1.2 * bmr_result
-       elif activity_level == 'light':
+      elif activity_level == 'light':
             activity_level = 1.375 * bmr_result
-       elif activity_level == 'moderate':
+      elif activity_level == 'moderate':
             activity_level = 1.55 * bmr_result
-       elif activity_level == 'heavy':
+      elif activity_level == 'heavy':
             activity_level = 1.725 * bmr_result
-       elif activity_level == 'extreme':
+      elif activity_level == 'extreme':
             activity_level = 1.9 * bmr_result
 
     	#return(int(activity_level))
 
      #def gain_or_lose(activity_level):
-       goals = st.text_input("Do you want to lose, maintain, or gain weight: ")
+      goals = st.text_input("Do you want to lose, maintain, or gain weight: ")
 
-       if goals == 'lose':
+      if goals == 'lose':
            calories = activity_level - 500
            st.write('in order to ', goals, 'weight, your daily caloric goals should be', calories, '!')
-       elif goals == 'maintain':
+      elif goals == 'maintain':
            calories = activity_level
             st.write('in order to ', goals, 'weight, your daily caloric goals should be', calories, '!')
-       elif goals == 'gain':
+      elif goals == 'gain':
            gain = st.text_input("Gain 1 or 2 pounds per week? Enter 1 or 2: ")
       	
-       if gain == 1: 
+      if gain == 1: 
            calories = activity_level + 500
            st.write('in order to ', goals, 'weight, your daily caloric goals should be', calories, '!')
-       elif gain == 2:
+      elif gain == 2:
            calories = activity_level + 1000
 	   st.write('in order to ', goals, 'weight, your daily caloric goals should be', calories, '!')
 
