c07e3fea2bb75100b80903ac8f3fc6b395871348
sitisyafirah <94594600+sitisyafirah@users.noreply.github.com>
2021-12-06 17:22:38 +0800

    Update heartrisk.py

--- heartrisk.py
+++ heartrisk.py
@@ -23,7 +23,6 @@ if option=='CALENDAR':
 elif option=='BMI CALCULATOR':
       height = st.number_input("Enter your height in m: ")
       weight = st.number_input("Enter your weight in kg: ")
-	
       BMI = weight/((height)**2)
       st.write('Your BMi is: ',BMI)
       if BMI <= 18.5:
